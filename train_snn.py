289ec3a3d25e291740c40296d49f2334f8bdc095
MarieSarah <mullersarah92@gmail.com>
2019-09-08 13:07:58 +0200

    added network dataset and extraction to the training scrip snn

---
+++ train_snn.py
@@ -0,0 +1,83 @@
+import lib.snn as snn
+import  lib.spikeFileIO as io
+
+
+from datetime import datetime
+import numpy as np
+import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import Dataset, DataLoader
+import lib.snn as snn
+import zipfile
+import os
+
+# CONSTANTS
+
+USE_CUDA = torch.cuda.is_available()
+#
+
+netParams = snn.params('data/mnist/network.yaml')
+print(netParams)
+
+# Dataset definition
+class nmnistDataset(Dataset):
+    def __init__(self, datasetPath, sampleFile, samplingTime, sampleLength):
+        self.path = datasetPath
+        self.samples = np.loadtxt(sampleFile).astype('int')
+        self.samplingTime = samplingTime
+        self.nTimeBins = int(sampleLength / samplingTime)
+
+    def __getitem__(self, index):
+        inputIndex = self.samples[index, 0]
+        classLabel = self.samples[index, 1]
+
+        inputSpikes = snn.io.read2Dspikes(
+            self.path + str(inputIndex.item()) + '.bs2'
+        ).toSpikeTensor(torch.zeros((2, 34, 34, self.nTimeBins)),
+                        samplingTime=self.samplingTime)
+        desiredClass = torch.zeros((10, 1, 1, 1))
+        desiredClass[classLabel, ...] = 1
+        return inputSpikes, desiredClass, classLabel
+
+    def __len__(self):
+        return self.samples.shape[0]
+
+# Network definition
+class Network(torch.nn.Module):
+    def __init__(self, netParams):
+        super(Network, self).__init__()
+        # initialize slayer
+        slayer = snn.layer(netParams['neuron'], netParams['simulation'])
+        self.slayer = slayer
+        # define network functions
+        self.conv1 = slayer.conv(2, 16, 5, padding=1)
+        self.conv2 = slayer.conv(16, 32, 3, padding=1)
+        self.conv3 = slayer.conv(32, 64, 3, padding=1)
+        self.pool1 = slayer.pool(2)
+        self.pool2 = slayer.pool(2)
+        self.fc1   = slayer.dense((8, 8, 64), 10)
+
+    def forward(self, spikeInput):
+        spikeLayer1 = self.slayer.spike(self.conv1(self.slayer.psp(spikeInput ))) # 32, 32, 16
+        spikeLayer2 = self.slayer.spike(self.pool1(self.slayer.psp(spikeLayer1))) # 16, 16, 16
+        spikeLayer3 = self.slayer.spike(self.conv2(self.slayer.psp(spikeLayer2))) # 16, 16, 32
+        spikeLayer4 = self.slayer.spike(self.pool2(self.slayer.psp(spikeLayer3))) #  8,  8, 32
+        spikeLayer5 = self.slayer.spike(self.conv3(self.slayer.psp(spikeLayer4))) #  8,  8, 64
+        spikeOut    = self.slayer.spike(self.fc1  (self.slayer.psp(spikeLayer5))) #  10
+
+        return spikeOut
+
+
+def extract_dataset(path='./',dataset_path= './data/mnist/NMNISTsmall.zip'):
+    # Extract NMNIST samples
+    with zipfile.ZipFile(dataset_path) as zip_file:
+        for member in zip_file.namelist():
+            if not os.path.exists(os.path.join(path,member)):
+                zip_file.extract(member, path)
+            else:
+                print('extraction path already exist', os.path.join(path,member))
+
+
+# extract_dataset()
+
+device = torch.device("cuda" if USE_CUDA else "cpu")
