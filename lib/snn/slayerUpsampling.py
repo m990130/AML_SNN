9cc403ad095a0825c79aa7fe1f04e7e4da84b7a2
Ariel Iporre <ariel.iporre.rivas@gmail.com>
2019-09-18 17:28:29 +0200

    upsampling class for spikes

--- lib/snn/slayerUpsampling.py
+++ lib/snn/slayerUpsampling.py
@@ -1,41 +1,28 @@
 from torch import nn
+from torch import functional as F
 
-
-class UpSampling2D(nn.Conv3d):
-    def __init__(self, inFeatures, outFeatures):
+class UpSampling2D(nn.Upsample):
+    def __init__(self, size=None, scale_factor=None, mode='nearest', align_corners=None):
         '''
         '''
-        # extract information for kernel and inChannels
-        if type(inFeatures) == int:
-            kernel = (1, 1, 1)
-            inChannels = inFeatures
-        elif len(inFeatures) == 2:
-            kernel = (inFeatures[1], inFeatures[0], 1)
-            inChannels = 1
-        elif len(inFeatures) == 3:
-            kernel = (inFeatures[1], inFeatures[0], 1)
-            inChannels = inFeatures[2]
-        else:
-            raise Exception('inFeatures should not be more than 3 dimension. It was: {}'.format(inFeatures.shape))
-        # print('Kernel Dimension:', kernel)
-        # print('Input Channels  :', inChannels)
+        super(UpSampling2D, self).__init__(size=size, scale_factor=scale_factor, mode=mode, align_corners=align_corners)
 
-        if type(outFeatures) == int:
-            outChannels = outFeatures
-        else:
-            raise Exception('outFeatures should not be more than 1 dimesnion. It was: {}'.format(outFeatures.shape))
-        # print('Output Channels :', outChannels)
-
-        super(_denseLayer, self).__init__(inChannels, outChannels, kernel, bias=False)
-
-        if weightScale != 1:
-            self.weight = torch.nn.Parameter(weightScale * self.weight)  # scale the weight if needed
-            # print('In dense, using weightScale of', weightScale)
+        # if weightScale != 1:
+        #     self.weight = torch.nn.Parameter(weightScale * self.weight)  # scale the weight if needed
+        #     # print('In dense, using weightScale of', weightScale)
 
     def forward(self, input):
         '''
         '''
-        return F.conv3d(input,
-                        self.weight, self.bias,
-                        self.stride, self.padding, self.dilation, self.groups)
+        # assert(len(input.shape)==5, 'Input expected to have 5 dimension, {} instead. '.format(input.shape))
+        batch, c, h, w, t = input.shape[0], input.shape[1],input.shape[2], input.shape[3],input.shape[4]
+
+        e = input.permute(0, 1, 4, 2, 3).contiguous().view(batch, c*t, h, w)
+        print(e.shape)
+        f = F.interpolate(e, self.size, self.scale_factor, self.mode, self.align_corners)
+        print(f.shape)
+        return f.view(batch, c, t, f.shape[-2], f.shape[-1]).permute(0,1,3,4,2).contiguous()
+
+
+
 # class Conv2DTranspose(object):
