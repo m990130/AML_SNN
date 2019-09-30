0c27be587adc63952b55b65a35e892b526403f24
Ariel Iporre <ariel.iporre.rivas@gmail.com>
2019-09-30 23:36:43 +0200

    add filter to the PCA

--- plot_pca_cnn.py
+++ plot_pca_cnn.py
@@ -8,6 +8,7 @@ from lib.process.Training_CNN import Training
 from mpl_toolkits.mplot3d import Axes3D
 from tqdm import tqdm
 from itertools import combinations
+from scipy.signal import savgol_filter
 
 import lib.snn as snn
 
@@ -21,7 +22,7 @@ EPOCHs = 6
 SMALL = False
 DATASETMODE = 'classification'
 MODEL_PTH = 'cnn'
-TRAIN = True
+TRAIN = False
 
 if TRAIN:
     device = torch.device("cuda" if USE_CUDA else "cpu")
@@ -50,101 +51,105 @@ if TRAIN:
             # Update training stats.
             stats.training.update()
 
-# load weights
-# for momentum in np.arange(0, 0.99, 0.1):
-#     model = Raw_CNN()
-#     with open('weights' + str(model.__class__) + '_' + str(momentum) + '.p', 'rb') as f:
-#         wc = pickle.load(f)
-#
-#     comps = wc.get_components(12)
-#     loss = wc.loss
-#     N = len(loss)
-#
-#     mpl.rcParams['legend.fontsize'] = 10
-#     print('plotting tuples vs loss\n')
-#     for (pc1, pc2) in tqdm(combinations(range(0, 12), 2)):
-#         z = - np.array(loss)
-#         x = comps[:, pc1]
-#         y = comps[:, pc2]
-#         fig = plt.figure()
-#         ax = fig.gca(projection='3d')
-#         ax.set_xlabel('pc_{}'.format(pc1))
-#         ax.set_ylabel('pc_{}'.format(pc2))
-#         ax.set_zlabel('negative loss')
-#
-#
-#         points = np.array([x, y, z]).T.reshape(-1, 1, 3)
-#         segments = np.concatenate([points[:-1], points[1:]], axis=1)
-#
-#         # Create a continuous norm to map from data points to colors
-#         norm = (z-z.min())/(z.max()-z.min())
-#         cmap = plt.get_cmap('plasma')
-#         colors = [cmap(norm[ii]) for ii in range(N-1)]
-#
-#         for ii in range(N-1):
-#             segii = segments[ii]
-#             lii, = ax.plot(segii[:, 0], segii[:, 1], segii[:, 2], color=colors[ii], linewidth=2)
-#             # lii.set_dash_joinstyle('round')
-#             # lii.set_solid_joinstyle('round')
-#             lii.set_solid_capstyle('round')
-#
-#         cmap = mpl.cm.plasma
-#         norm = mpl.colors.Normalize(vmin=z.min(), vmax=z.max())
-#
-#         cbaxes = fig.add_axes([0.025, 0.1, 0.03, 0.8])
-#         cb1 = mpl.colorbar.ColorbarBase(cbaxes, cmap=cmap,
-#                                         norm=norm,
-#                                         )
-#
-#         ax.set_xlim(x.min(), x.max())
-#         ax.set_ylim(y.min()-0.01, y.max()+0.01)
-#
-#         # plt.show()
-#         fig.savefig(
-#             './saved_figs/pca_' + str(pc1) + '_' + str(pc2) + '_' + str(momentum) + '_' + str(model.__class__) + '.pdf')
-#         plt.close(fig)
-#
-#     print('printing triplets\n')
-#     for (pc1, pc2, pc3) in tqdm(combinations(range(0, 12), 3)):
-#         x = comps[:, pc1]
-#         y = comps[:, pc2]
-#         z = comps[:, pc3]
-#         t = -np.array(loss)
-#
-#         fig = plt.figure()
-#         ax = fig.gca(projection='3d')
-#         ax.set_xlabel('pc_{}'.format(pc1))
-#         ax.set_ylabel('pc_{}'.format(pc2))
-#         ax.set_zlabel('pc_{}'.format(pc3))
-#
-#
-#         points = np.array([x, y, z]).T.reshape(-1, 1, 3)
-#         segments = np.concatenate([points[:-1], points[1:]], axis=1)
-#
-#         # Create a continuous norm to map from data points to colors
-#         norm = (t-t.min())/(t.max()-t.min())
-#         cmap = plt.get_cmap('plasma')
-#         colors = [cmap(norm[ii]) for ii in range(N-1)]
-#
-#         for ii in range(N-1):
-#             segii = segments[ii]
-#             lii, = ax.plot(segii[:, 0], segii[:, 1], segii[:, 2], color=colors[ii], linewidth=2)
-#             # lii.set_dash_joinstyle('round')
-#             # lii.set_solid_joinstyle('round')
-#             lii.set_solid_capstyle('round')
-#
-#         cmap = mpl.cm.plasma
-#         norm = mpl.colors.Normalize(vmin=t.min(), vmax=t.max())
-#
-#         cbaxes = fig.add_axes([0.025, 0.1, 0.03, 0.8])
-#         cb1 = mpl.colorbar.ColorbarBase(cbaxes, cmap=cmap,
-#                                         norm=norm,
-#                                         )
-#
-#         ax.set_xlim(x.min(), x.max())
-#         ax.set_ylim(y.min()-0.01, y.max()+0.01)
-#
-#         # plt.show()
-#         fig.savefig(
-#             './saved_figs/pca_' + str(pc1) + '_' + str(pc2) + '_' + str(pc3) + '_' + str(momentum) + '_' + str(model.__class__) + '.pdf')
-#         plt.close(fig)
+## load weights
+for momentum in [ 0.2, 0.9]: #np.arange(0, 0.99, 0.1):
+    model = Raw_CNN()
+    with open('/Volumes/GG/cnn/weights' + str(model.__class__) + '_' + str(momentum) + '.p', 'rb') as f:
+        wc = pickle.load(f)
+
+    comps = wc.get_components(12)
+    loss = wc.loss
+    N = len(loss)
+
+    mpl.rcParams['legend.fontsize'] = 10
+    print('plotting tuples vs loss\n')
+    for (pc1, pc2) in tqdm(combinations(range(0, 12), 2)):
+        z = - np.array(loss)
+        z = savgol_filter(z, 51, 3)  # window size 51, polynomial order 3
+
+        x = comps[:, pc1]
+        y = comps[:, pc2]
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.set_xlabel('pc_{}'.format(pc1))
+        ax.set_ylabel('pc_{}'.format(pc2))
+        ax.set_zlabel('negative loss')
+
+
+        points = np.array([x, y, z]).T.reshape(-1, 1, 3)
+        segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+        # Create a continuous norm to map from data points to colors
+        norm = (z-z.min())/(z.max()-z.min())
+        cmap = plt.get_cmap('plasma')
+        colors = [cmap(norm[ii]) for ii in range(N-1)]
+
+        for ii in range(N-1):
+            segii = segments[ii]
+            lii, = ax.plot(segii[:, 0], segii[:, 1], segii[:, 2], color=colors[ii], linewidth=2)
+            # lii.set_dash_joinstyle('round')
+            # lii.set_solid_joinstyle('round')
+            lii.set_solid_capstyle('round')
+
+        cmap = mpl.cm.plasma
+        norm = mpl.colors.Normalize(vmin=z.min(), vmax=z.max())
+
+        cbaxes = fig.add_axes([0.025, 0.1, 0.03, 0.8])
+        cb1 = mpl.colorbar.ColorbarBase(cbaxes, cmap=cmap,
+                                        norm=norm,
+                                        )
+
+        ax.set_xlim(x.min(), x.max())
+        ax.set_ylim(y.min()-0.01, y.max()+0.01)
+
+        # plt.show()
+        print('/Volumes/GG/cnn/loss/pca_' + str(pc1) + '_' + str(pc2) + '_' + str(momentum) + '_' + str(model.__class__.__name__) + '.pdf')
+        fig.savefig(
+            '/Volumes/GG/cnn/loss/pca_' + str(pc1) + '_' + str(pc2) + '_' + str(momentum) + '_' + str(model.__class__.__name__) + '.pdf')
+        plt.close(fig)
+
+    print('printing triplets\n')
+    for (pc1, pc2, pc3) in tqdm(combinations(range(0, 12), 3)):
+        x = comps[:, pc1]
+        y = comps[:, pc2]
+        z = comps[:, pc3]
+        t = -np.array(loss)
+
+        fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        ax.set_xlabel('pc_{}'.format(pc1))
+        ax.set_ylabel('pc_{}'.format(pc2))
+        ax.set_zlabel('pc_{}'.format(pc3))
+
+
+        points = np.array([x, y, z]).T.reshape(-1, 1, 3)
+        segments = np.concatenate([points[:-1], points[1:]], axis=1)
+
+        # Create a continuous norm to map from data points to colors
+        norm = (t-t.min())/(t.max()-t.min())
+        cmap = plt.get_cmap('plasma')
+        colors = [cmap(norm[ii]) for ii in range(N-1)]
+
+        for ii in range(N-1):
+            segii = segments[ii]
+            lii, = ax.plot(segii[:, 0], segii[:, 1], segii[:, 2], color=colors[ii], linewidth=2)
+            # lii.set_dash_joinstyle('round')
+            # lii.set_solid_joinstyle('round')
+            lii.set_solid_capstyle('round')
+
+        cmap = mpl.cm.plasma
+        norm = mpl.colors.Normalize(vmin=t.min(), vmax=t.max())
+
+        cbaxes = fig.add_axes([0.025, 0.1, 0.03, 0.8])
+        cb1 = mpl.colorbar.ColorbarBase(cbaxes, cmap=cmap,
+                                        norm=norm,
+                                        )
+
+        ax.set_xlim(x.min(), x.max())
+        ax.set_ylim(y.min()-0.01, y.max()+0.01)
+
+        # plt.show()
+        print('/Volumes/GG/cnn/triplets/pca_' + str(pc1) + '_' + str(pc2) + '_' + str(pc3) + '_' + str(momentum) + '_' + str(model.__class__.__name__) + '.pdf')
+        fig.savefig(
+            '/Volumes/GG/cnn/triplets/pca_' + str(pc1) + '_' + str(pc2) + '_' + str(pc3) + '_' + str(momentum) + '_' + str(model.__class__.__name__) + '.pdf')
+        plt.close(fig)
